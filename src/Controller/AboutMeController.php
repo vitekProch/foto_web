2bcc5df28c031ea4cd42ed7890c23008f9e9fe65
Vitalij Prochazka <vitek.prochazka1@gmail.com>
2023-10-12 21:01:21 +0200

    Connect twig to controller

--- src/Controller/AboutMeController.php
+++ src/Controller/AboutMeController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class AboutMeController extends AbstractController
 {
     #[Route('/about-me')]
-    public function aboutMePage()
+    public function aboutMePage(): Response
     {
-        die('Chello Word');
+        return $this->render('AboutMe/about_me_page.html.twig', [
+            'title' => 'O mně',
+        ]);
     }
 }
