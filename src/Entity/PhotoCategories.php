3fb62261456b0673f741a64d9530b5c337afe483
Vitalij Prochazka <vitek.prochazka1@gmail.com>
2023-10-20 16:34:35 +0200

    Photo categories

---
+++ src/Entity/PhotoCategories.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhotoCategoriesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PhotoCategoriesRepository::class)]
+class PhotoCategories
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $categoryName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $categoryPhotoPath = null;
+
+    #[ORM\Column]
+    private \DateTimeImmutable $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCategoryName(): ?string
+    {
+        return $this->categoryName;
+    }
+
+    public function setCategoryName(string $categoryName): static
+    {
+        $this->categoryName = $categoryName;
+
+        return $this;
+    }
+
+    public function getCategoryPhotoPath(): ?string
+    {
+        return $this->categoryPhotoPath;
+    }
+
+    public function setCategoryPhotoPath(string $categoryPhotoPath): static
+    {
+        $this->categoryPhotoPath = $categoryPhotoPath;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
