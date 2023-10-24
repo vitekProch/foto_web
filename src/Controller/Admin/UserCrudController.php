8d4c91d05280818a96fe22f607e55e96280ac8f4
Vitalij Prochazka <vitek.prochazka1@gmail.com>
2023-10-24 17:52:12 +0200

    Change showed data

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,10 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -19,10 +22,15 @@ class UserCrudController extends AbstractCrudController
     
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id', 'Identifikační číslo'),
-            TextField::new('username' , 'Jméno'),
-        ];
+        yield IdField::new('id', 'Identifikační číslo')
+            ->onlyOnIndex();
+        yield TextField::new('username' , 'Jméno');
+        $roles = ['ROLE_ADMIN', 'ROLE_USER'];
+        yield ChoiceField::new('roles')
+            ->setChoices(array_combine($roles, $roles))
+            ->allowMultipleChoices()
+            ->renderExpanded()
+            ->renderAsBadges();
     }
     
 }
