429f6cf446ddc41a111d057dd15fc66be4434395
Vitalij Prochazka <vitek.prochazka1@gmail.com>
2023-11-05 19:34:25 +0100

    Header on homepage function

--- assets/controllers/navbar_controller.js
+++ assets/controllers/navbar_controller.js
@@ -1,5 +1,28 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-
+    static targets = ["navbarBackground", "toggleButton"];
+    connect(){
+        if (window.location.pathname === "/"){
+            this.navbarBackgroundTarget.classList.remove("bg-body-tertiary");
+        }
+    }
+    navbarHamburgerClicked(){
+        let ariaExpanded = this.toggleButtonTarget.getAttribute("aria-expanded")
+        if (ariaExpanded){
+            this.navbarBackgroundTarget.classList.add("bg-body-tertiary");
+        }
+        if ((window.location.pathname === "/") && ariaExpanded === "false" && (window.scrollY === 0)){
+            this.navbarBackgroundTarget.classList.remove("bg-body-tertiary");
+        }
+    }
+    navbarBackgroundShow(){
+        let ariaExpanded = this.toggleButtonTarget.getAttribute("aria-expanded")
+        if ((window.location.pathname === "/") && (ariaExpanded === "false") && (window.scrollY === 0)){
+            this.navbarBackgroundTarget.classList.remove("bg-body-tertiary");
+        }
+        if ((window.scrollY > 1) || (ariaExpanded === "true")){
+            this.navbarBackgroundTarget.classList.add("bg-body-tertiary");
+        }
+    }
 }
