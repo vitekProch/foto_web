6ae26849fab4fd07073bc06b9b31bec16735740f
Vitalij Prochazka <vitek.prochazka1@gmail.com>
2023-11-20 02:16:05 +0100

    Portfolio template with categories

--- src/Controller/PortfolioPageController.php
+++ src/Controller/PortfolioPageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\PhotoCategories;
+use App\Repository\PhotoCategoriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,9 +11,11 @@ use Symfony\Component\HttpFoundation\Response;
 class PortfolioPageController extends AbstractController
 {
     #[Route('/portfolio', name: "portfolio_page")]
-    public function portfolioPage(): Response
+    public function portfolioPage(PhotoCategoriesRepository $photoCategoryRepository): Response
     {
+        $categories = $photoCategoryRepository->findAll();
         return $this->render('PortfolioPage/portfolio_page.html.twig', [
+            'categories' => $categories,
             'title' => 'Portfolio',
         ]);
     }
