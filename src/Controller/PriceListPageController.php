70d38b1f57616580b12473ef4a6687367f1b7283
Vitalij Prochazka <vitek.prochazka1@gmail.com>
2024-02-09 01:27:28 +0100

    created PriceList table

--- src/Controller/PriceListPageController.php
+++ src/Controller/PriceListPageController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+
+use App\Repository\PhotoPackageNamesRepository;
+use App\Repository\WeddingPhotoPackagesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,10 +12,14 @@ use Symfony\Component\HttpFoundation\Response;
 class PriceListPageController extends AbstractController
 {
     #[Route('/price-list', name: 'price_list_page')]
-    public function priceListPage(): Response
+    public function priceListPage(PhotoPackageNamesRepository $packageNamesRepository, WeddingPhotoPackagesRepository $weddingPhotoPackagesRepository): Response
     {
+        $data = $weddingPhotoPackagesRepository->findAll();
+
         return $this->render('PriceListPage/price_list_page.html.twig', [
             'title' => 'Ceník',
+            'smallPriceList' => $packageNamesRepository->findAll(),
+            'weddingPriceList' => $weddingPhotoPackagesRepository->findAll(),
         ]);
     }
 }
