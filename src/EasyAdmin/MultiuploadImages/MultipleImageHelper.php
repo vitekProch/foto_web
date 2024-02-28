a637e3e20126046db47a612b8100c4b58a8753c6
Vitalij Procházka <vitek.prochazka1@seznam.cz>
2024-02-28 15:04:47 +0100

    refactor code

--- src/EasyAdmin/MultiuploadImages/MultipleImageHelper.php
+++ src/EasyAdmin/MultiuploadImages/MultipleImageHelper.php
@@ -1,36 +1,35 @@
 <?php
 
 namespace App\EasyAdmin\MultiUploadImages;
+
 use Symfony\Component\HttpKernel\KernelInterface;
 
 class MultipleImageHelper
 {
-    private $appKernel;
+    private KernelInterface $appKernel;
 
     public function __construct(KernelInterface $appKernel)
     {
         $this->appKernel = $appKernel;
-
     }
-    function generateUniqueFileName(string $currentFileName): string
+
+    public function generateUniqueFileName(string $currentFileName): string
     {
-        $fileSplit = explode( '.', $currentFileName );
+        $fileSplit = explode('.', $currentFileName);
         return $fileSplit[0] . '-' . md5(microtime())  . '.' . end($fileSplit);
     }
 
     public function deletePhotoFromDirectory(string $reviewToRemovePath): void
     {
-        $deleteImgPath = $this->appKernel->getProjectDir().
+        $deleteImgPath = $this->appKernel->getProjectDir() .
             '/public' . $reviewToRemovePath;
-        if(file_exists($deleteImgPath)) unlink($deleteImgPath);
+        if (file_exists($deleteImgPath)) {
+            unlink($deleteImgPath);
+        }
     }
 
     public function upload($file, $uniqueImageName, $uploadDirectory): void
     {
-        $file->move(
-            $uploadDirectory,
-            $uniqueImageName
-        );
+        $file->move($uploadDirectory, $uniqueImageName);
     }
-
 }
