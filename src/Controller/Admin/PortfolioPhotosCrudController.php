e3be5385150e35e9ce10e9f30847f05797fc6c74
Vitalij Procházka <vitek.prochazka1@seznam.cz>
2024-02-28 15:36:12 +0100

    code refactor

--- src/Controller/Admin/PortfolioPhotosCrudController.php
+++ src/Controller/Admin/PortfolioPhotosCrudController.php
@@ -3,7 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\EasyAdmin\Fields\MultipleImageField;
-use App\EasyAdmin\MultiUploadImages\MultipleImagesHelper;
+use App\EasyAdmin\MultiUploadImages\MultiUploadImages;
 use App\Entity\PortfolioPhotos;
 use App\Repository\PortfolioPhotosRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -19,14 +19,14 @@ use Symfony\Component\HttpFoundation\Response;
 class PortfolioPhotosCrudController extends AbstractCrudController
 {
     private PortfolioPhotosRepository $photosRepository;
-    private MultipleImagesHelper $uploadHelper;
+    private MultiUploadImages $uploadHelper;
 
     public static function getEntityFqcn(): string
     {
         return PortfolioPhotos::class;
     }
 
-    public function __construct(PortfolioPhotosRepository $photosRepository, MultipleImagesHelper $uploadHelper)
+    public function __construct(PortfolioPhotosRepository $photosRepository, MultiUploadImages $uploadHelper)
     {
         $this->photosRepository = $photosRepository;
         $this->uploadHelper = $uploadHelper;
--- src/EasyAdmin/MultiuploadImages/MultiUploadImages.php
+++ src/EasyAdmin/MultiuploadImages/MultiUploadImages.php
@@ -4,7 +4,7 @@ namespace App\EasyAdmin\MultiUploadImages;
 
 use Symfony\Component\HttpKernel\KernelInterface;
 
-class MultipleImagesHelper
+class MultiUploadImages
 {
     private KernelInterface $appKernel;
 
